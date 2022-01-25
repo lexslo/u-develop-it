4c053cbcc9bf2e5d332c152d69a2533db9dd757d
Sparkle Party <lexi.slovik@gmail.com>
2022-01-25 11:46:13 -0800

    connect server to SQL database

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2');
+const { user, password } = require("./creds");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +8,21 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: user,
+        password: password,
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // PLACE BELOW ALL GET ROUTES
 // Default response for any other request (Not Found)
