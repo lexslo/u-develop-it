c0a26082b95e66a9d1b83417660a9a58cf9bf585
Sparkle Party <lexi.slovik@gmail.com>
2022-01-30 16:42:22 -0800

    add all voter routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use(require('./candidate-routes'));
 router.use(require('./party-routes'));
+router.use(require('./voter-routes'));
 
 module.exports = router;
